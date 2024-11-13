63a3e5b61e4eed417933841778def3b7fb1509d4
Arcarius-code <2011jisung@gmail.com>
2024-11-14 00:06:53 +0900

    Add files via upload

--- RunPy.cs
+++ RunPy.cs
@@ -7,42 +7,38 @@ public class RunPy : MonoBehaviour
 {
     void Start()
     {
-        UnityEngine.Debug.Log("Hello world!");
+        try {
+            Process psi = new Process();
+            UnityEngine.Debug.Log("ProcessLaunched");
+            psi.StartInfo.FileName = "C:\\ProgramData\\anaconda3\\python.exe";
+            // 시작할 어플리케이션 또는 문서
+            UnityEngine.Debug.Log("PythonLaunched");
+            psi.StartInfo.Arguments = "C:\\Users\\2011j\\EE101\\4_4_1_for_c#.py";
+            // 애플 시작시 사용할 인수
+            UnityEngine.Debug.Log("ProjLaunched");
+            psi.StartInfo.CreateNoWindow = false;
+            // 새창 안띄울지
+            psi.StartInfo.RedirectStandardOutput = true;
+            // python.exe에서 출력되는 standardoutput을 redirection
+            psi.StartInfo.UseShellExecute = false;
+            // 프로세스를 시작할때 운영체제 셸을 사용할지
+            psi.Start();     
+                
+            while (!psi.StandardOutput.EndOfStream)
+            {
+                string line = psi.StandardOutput.ReadLine(); 
+                //python.exe에서 나온 output을 읽어서 출력
+                UnityEngine.Debug.Log(line);
+            }
+        }
+        catch(Exception e)
+        {
+            UnityEngine.Debug.LogError("Unable to launch app: " + e.Message);
+        }
+
     }
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            try {
-                Process psi = new Process();
-                UnityEngine.Debug.Log("ProcessLaunched");
-                psi.StartInfo.FileName = "C:\\ProgramData\\anaconda3\\python.exe";
-                // 시작할 어플리케이션 또는 문서
-                UnityEngine.Debug.Log("PythonLaunched");
-                psi.StartInfo.Arguments = "C:\\Users\\2011j\\EE101\\4_4_1_for_c#.py";
-                // 애플 시작시 사용할 인수
-                UnityEngine.Debug.Log("ProjLaunched");
-                psi.StartInfo.CreateNoWindow = false;
-                psi.StartInfo.RedirectStandardOutput = true;
-                // 새창 안띄울지
-                psi.StartInfo.UseShellExecute = false;
-                // 프로세스를 시작할때 운영체제 셸을 사용할지
-                psi.Start();     
-                   
-                while (!psi.StandardOutput.EndOfStream)
-                {
-                    UnityEngine.Debug.Log("실행중");
-                    string line = psi.StandardOutput.ReadLine();
-                    // do something with line
-                    UnityEngine.Debug.Log(line);
-                }
-                UnityEngine.Debug.Log("실행");
-            }
-            catch(Exception e)
-            {
-                UnityEngine.Debug.LogError("Unable to launch app: " + e.Message);
-            }
-        }
+        
     }
 }
